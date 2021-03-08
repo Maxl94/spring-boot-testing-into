4a6cb2f37882186430bbc04893a0ec1d0ac47b2d
ballufma <maximilian.balluff@pentasys.de>
2021-03-08 17:08:25 +0100

    Revert "Initial commit, added employee with additional mapping layer, added service and controller"
    This reverts commit c92f10b4

--- src/main/java/com/ausytechnologies/testingintro/persistence/model/util/Status.java
+++
@@ -1,7 +0,0 @@
-package com.ausytechnologies.testingintro.persistence.model.util;
-
-public enum Status {
-    ACTIVE,
-    RETIRED,
-    SABBATICAL
-}
--- src/main/java/com/ausytechnologies/testingintro/persistence/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ausytechnologies.testingintro.persistence.repository;
-
-import com.ausytechnologies.testingintro.persistence.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
